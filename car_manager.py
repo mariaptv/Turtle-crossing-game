391b61362a8b679e8369ba449a2fee309ba4074e
mariaptv <mariaptellezv@gmail.com>
2024-09-20 21:50:46 -0500

    First

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+import random
+
+from TurtleGame.turtle_race import position
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.color(random.choice(COLORS))
+        self.position = random.randint(-300, 300)
+        self.goto(position, -300)
--- main.py
+++ main.py
@@ -6,8 +6,15 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Pong")
 screen.tracer(0)
 
+turtle = Player()
+for i in range(16):
+    car = CarManager()
+screen.listen()
+
+screen.onkey(turtle.move, "Up")
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
